92397e7ef29bbcde46e312a37bc10f03eade3fe0
alexander-richard <35641591+alexander-richard@users.noreply.github.com>
2020-12-01 15:27:51 -0700

    Modified the labels

--- ring_simulation/script.js
+++ ring_simulation/script.js
@@ -373,26 +373,21 @@ function create_animation(k) {
   //c.fillText("Iteration: " + k, 20, 30); // uncomment to debug
 
   // draw the legend
-  let legend_offset_x = cvs.width - 220;
+  let legend_offset_x = cvs.width - 250;
   let legend_offset_y = 30;
   c.font = "23px Arial";
-  c.fillText("Running Node", legend_offset_x, legend_offset_y + 20);
-  c.fillText("Elected Leader", legend_offset_x, legend_offset_y + 50);
-  c.fillText("Running for Election", legend_offset_x, legend_offset_y + 80);
-  c.fillText("Crashed Node", legend_offset_x, legend_offset_y + 110);
-
-  c.fillStyle = RUNNING_PROCESS;
-  c.fillRect(legend_offset_x - 50, legend_offset_y + 20, 40, -20);
+  c.fillText("Elected Leader", legend_offset_x, legend_offset_y + 20);
+  c.fillText("Requesting an Election", legend_offset_x, legend_offset_y + 50);
+  c.fillText("Crashed", legend_offset_x, legend_offset_y + 80);
 
   c.fillStyle = BECOME_LEADER;
-  c.fillRect(legend_offset_x - 50, legend_offset_y + 50, 40, -20);
+  c.fillRect(legend_offset_x - 50, legend_offset_y + 20, 40, -20);
 
   c.fillStyle = CALL_ELECTION;
-  c.fillRect(legend_offset_x - 50, legend_offset_y + 80, 40, -20);
+  c.fillRect(legend_offset_x - 50, legend_offset_y + 50, 40, -20);
 
   c.fillStyle = CRASHED;
-  c.fillRect(legend_offset_x - 50, legend_offset_y + 110, 40, -20);
-
+  c.fillRect(legend_offset_x - 50, legend_offset_y + 80, 40, -20);
 
   // draw the connections
   c.strokeStyle = 'rgba(0, 0, 0, 1)';
